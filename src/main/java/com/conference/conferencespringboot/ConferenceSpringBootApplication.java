397e622aaf20404de3f15207b8aeedbdf49f4955
trystan.newbury <trystan.newbury@babylonhealth.com>
2020-06-02 15:42:16 +0100

    Create Spring Boot conference application using Spring Initializr

---
+++ src/main/java/com/conference/conferencespringboot/ConferenceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.conference.conferencespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conference/conferencespringboot/ConferenceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conference.conferencespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
