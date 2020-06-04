297f71f186dd6f54cb67d33b744377b3554c0df2
trystan.newbury <trystan.newbury@gmail.com>
2020-06-04 15:05:23 +0100

    Create Speaker JPA model, define many-to-many relationship between the Speaker and Session entities and define the binary data for the speaker photo attribute

--- src/main/java/com/conference/conferencespringboot/models/Session.java
+++ src/main/java/com/conference/conferencespringboot/models/Session.java
@@ -1,18 +1,31 @@
 package com.conference.conferencespringboot.models;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity(name = "sessions")
 public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long session_id;
+
     private String session_name;
     private String session_description;
     private Integer session_length;
+
+    @ManyToMany
+    @JoinTable(
+        name = "session_speakers",
+        joinColumns = @JoinColumn(name = "session_id"),
+        inverseJoinColumns = @JoinColumn(name = "speaker_id")
+    )
+    private List<Speaker> speakers;
 
     public Session() {
 
@@ -48,5 +61,13 @@ public class Session {
 
     public void setSession_length(Integer session_length) {
         this.session_length = session_length;
+    }
+
+    public List<Speaker> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<Speaker> speakers) {
+        this.speakers = speakers;
     }
 }
