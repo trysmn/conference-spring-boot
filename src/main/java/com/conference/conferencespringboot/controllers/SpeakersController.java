66b15c1f44edb9e613df5446b682e427976c27a0
trystan.newbury <trystan.newbury@gmail.com>
2020-06-04 17:39:44 +0100

    Create controllers for the Speaker and Session entities, with the list, get and create methods implemented

---
+++ src/main/java/com/conference/conferencespringboot/controllers/SpeakersController.java
@@ -0,0 +1,38 @@
+package com.conference.conferencespringboot.controllers;
+
+import com.conference.conferencespringboot.models.Speaker;
+import com.conference.conferencespringboot.repositories.SpeakerRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Speaker create(@RequestBody final Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+}
