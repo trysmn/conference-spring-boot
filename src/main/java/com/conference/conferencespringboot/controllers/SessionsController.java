f8a3c6b6fab5458ed3aeee49f37d22d47f3f201d
trystan.newbury <trystan.newbury@gmail.com>
2020-06-08 10:48:25 +0100

    Add update endpoint for SessionsController and SpeakersController

--- src/main/java/com/conference/conferencespringboot/controllers/SessionsController.java
+++ src/main/java/com/conference/conferencespringboot/controllers/SessionsController.java
@@ -3,6 +3,7 @@ package com.conference.conferencespringboot.controllers;
 import com.conference.conferencespringboot.models.Session;
 import com.conference.conferencespringboot.repositories.SessionRepository;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,18 @@ public class SessionsController {
     @ResponseStatus(HttpStatus.CREATED)
     public Session create(@RequestBody final Session session) {
         return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session) {
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 Bad Payload status.
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 }
--- src/main/java/com/conference/conferencespringboot/controllers/SpeakersController.java
+++ src/main/java/com/conference/conferencespringboot/controllers/SpeakersController.java
@@ -3,6 +3,7 @@ package com.conference.conferencespringboot.controllers;
 import com.conference.conferencespringboot.models.Speaker;
 import com.conference.conferencespringboot.repositories.SpeakerRepository;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,18 @@ public class SpeakersController {
     @ResponseStatus(HttpStatus.CREATED)
     public Speaker create(@RequestBody final Speaker speaker) {
         return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        // TODO: Add validation that all attributes are passed in, otherwise return a 400 Bad Payload status.
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
